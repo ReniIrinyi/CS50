cd6d6f826051f192edbf8556b76a007663a719fd
RenataIrinyi <reniirinyi@icloud.com>
2023-04-05 10:23:46 +0200

    week1

--- Week1/hello.c
+++ Week1/hello.c
@@ -1,8 +1,19 @@
 
 #include <stdio.h>
 
+
 int main(void){
 
-  printf("hello world! \n");
+
+  int counter=3;
+while(counter>0){
+  printf("meow\n");
+  counter--;
+}
+
+for (int i=0; i<15; i++) {
+  printf("meow meow\n");
+}
 
 }
+
