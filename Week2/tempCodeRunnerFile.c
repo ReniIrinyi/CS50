5d251a6f0c35fff2de812585f1d348a28246cbe8
RenataIrinyi <reniirinyi@icloud.com>
2023-04-17 10:22:23 +0200

    week2

--- Week2/tempCodeRunnerFile.c
+++ Week2/tempCodeRunnerFile.c
@@ -1,2 +1,9 @@
-/* constant-expression */:
-            /* code */
+  // int score2=compute_score("maus");
+
+
+    // if(score1>score2){
+    //     printf("Player 1 wins: %i!", compute_score("cat"));
+    // if(score1>score2){
+    //     printf("Player 2 wins!");
+    // } else printf("Tie!");
+    // }
--- tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
-  // int score2=compute_score("maus");
-
-
-    // if(score1>score2){
-    //     printf("Player 1 wins: %i!", compute_score("cat"));
-    // if(score1>score2){
-    //     printf("Player 2 wins!");
-    // } else printf("Tie!");
-    // }
