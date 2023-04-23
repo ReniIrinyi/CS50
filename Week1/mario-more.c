1939b845d159fd5c4ca15eb4d00b7386f98f20eb
RenataIrinyi <reniirinyi@icloud.com>
2023-04-23 16:28:50 +0200

    make mario recursive

--- Week1/mario-more.c
+++ Week1/mario-more.c
@@ -1,50 +1,59 @@
 
 #include <stdio.h>
-
+void printPyramid(int input);
+void printPyramid2(int input);
 int main(void)
 {
 int input=5;
+printPyramid(input); 
 
-//LESSS
-    for(int i = 1; i <= input; i++){
-        printf("\n");
-          for(int j = 1; j <= i; j++){
-            printf("#");
-          }
-    } 
+// //LESSS
+//     for(int i = 1; i <= input; i++){
+//         printf("\n");
+//           for(int j = 1; j <= i; j++){
+//             printf("#");
+//           }
+//     } 
 
-printf("\n------- \n"); 
+// printf("\n------- \n"); 
 
-     for(int i = input; i >= 1; i--){
-          for(int j = i; j > 1; j--){
-            printf(" ");
-          }
-          //printing the pyramid
-          for(int x=input; x>=i; x--){
-            printf("#");
-          }
-          printf("\n"); 
-    }
+//      for(int i = input; i >= 1; i--){
+//           for(int j = i; j > 1; j--){
+//             printf(" ");
+//           }
+//           //printing the pyramid
+//           for(int x=input; x>=i; x--){
+//             printf("#");
+//           }
+//           printf("\n"); 
+//     }
 
-printf("\n------- \n"); 
+// printf("\n------- \n"); 
  
  //MORE
- 
-  for(int i = input; i >= 1; i--){
-          for(int j = i; j > 1; j--){
-            printf(" ");
-          }
-          //printing the pyramid
-          for(int x=input; x>=i; x--){
-            printf("#");
-   
-          }
-                   printf("  ");
-              for(int j = i; j <=input; j++){
-            printf("#");
-          }
-          printf("\n"); 
-    }
-
-
+// for(int i = input; i >= 1; i--){
+//           for(int j = i; j > 1; j--){
+//             printf(" ");
+//           }
+//           //printing the pyramid
+//           for(int x=input; x>=i; x--){
+//             printf("#");
+//           }
+//                    printf("  ");
+//               for(int j = i; j <=input; j++){
+//             printf("#");
+//           }
+//           printf("\n"); 
+//     }
 }
+
+void printPyramid(int input)
+{
+   if(input-1 != 0){
+      printPyramid(input-1); 
+  }
+ for(int i = input; i >0; i--){
+      printf("#");    }
+  printf("\n"); 
+}
+ 
--- Week1/tempCodeRunnerFile.c
+++ Week1/tempCodeRunnerFile.c
@@ -1,5 +1,1 @@
-        printf("\n");
-          for(int j = 1; j <= i; j++){
-            printf("#");
-          }
-    } 
+int input=5;
