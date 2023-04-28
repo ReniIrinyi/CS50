ab02ed8a61688cc5e55e78a11870342a38b3abb0
RenataIrinyi <reniirinyi@icloud.com>
2023-04-28 10:28:23 +0200

    start week 4

---
+++ Week4/Smiley/helpers.h
@@ -0,0 +1,5 @@
+#include "/Users/renatairinyi/Documents/PROGRAMOZÀS/GitHub/Training---Harvard-CS50__Introduction-to-Computer-Science/Week4/Smiley/bmp.h"
+
+// colorize image
+void colorize(int height, int width, RGBTRIPLE image[height][width]);
+
